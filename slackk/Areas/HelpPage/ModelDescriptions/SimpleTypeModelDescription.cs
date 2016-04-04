834318dd8fae7242bc955df8c948d244a2ec51e6
Alireza <alireza.m373@gmail.com>
2016-04-04 17:45:14 +0430

    Cleanup

---
+++ slackk/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace slackk.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ slackk/Models/SlackResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace slackk.Models
+{
+    public class SlackResponse
+    {
+        public string OK { get; set; }
+        public string Channel { get; set; }
+        public string Error { get; set; }
+        public Message Message { get; set; }
+
+    }
+}
